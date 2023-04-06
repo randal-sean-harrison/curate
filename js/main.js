fff0c3cbadee180e8340c4bf2e4e8ea6b8fe23f3
Randal Sean Harrison <rharriso@nd.edu>
2023-04-06 06:42:22 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+   // Load header and add tooltips dyamically ------------------------
+  $("#header").load("header.html", function () {
+    console.log("Header loaded.");
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // Load footer -----------------------------------------------------
+  $("#footer").load("footer.html", function () {
+    console.log("Footer loaded.");
+  });
+
+
+
+});
+// document.ready
