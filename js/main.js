5bf26877c10279fbcdf1f5e7319d2035c41b175f
Randal Sean Harrison <rharriso@nd.edu>
2023-04-06 07:09:05 -0400

    updated footer, header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
    // Load header and add tooltips dyamically ------------------------
-  $("#header").load("header.html", function () {
+  $("header").load("header.html", function () {
     console.log("Header loaded.");
     $('[data-toggle="tooltip"]').tooltip();
   });
 
   // Load footer -----------------------------------------------------
-  $("#footer").load("footer.html", function () {
+  $("footer").load("footer.html", function () {
     console.log("Footer loaded.");
   });
 
