fc59a0864525da13fb7e5727ace4c4121fab6918
Randal Sean Harrison <rharriso@nd.edu>
2023-04-07 04:20:34 -0400

    updating the main template and css

--- js/main.js
+++ js/main.js
@@ -11,6 +11,51 @@ $(document).ready(function () {
     console.log("Footer loaded.");
   });
 
+// Slow scroll to clicked link ------------------------------------
+function scrollToElement(elementId) {
+  const element = $('#' + elementId);
+  const offsetTop = element.offset().top;
+  const scrollHeight = Math.abs($(window).scrollTop() - offsetTop);
+  const duration = Math.min(scrollHeight / 2, 1000); // medium speed
+
+  $('html, body').animate({
+    scrollTop: offsetTop
+  }, duration);
+}
+
+$('p[data-scroll-to]').on('click', function(event) {
+  event.preventDefault();
+  const elementId = $(this).data('scroll-to');
+  scrollToElement(elementId);
+});
+
+// Scroll to top button --------------------------------------------
+window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("topper").style.display = "block";
+    } else {
+      document.getElementById("topper").style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+  $("#topper").on("click", function () {
+    $("html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+  });
 
 
 });
