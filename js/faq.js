b9e2787eaffde0687bd330b7da9aa5d889d25e90
Randal Sean Harrison <rharriso@nd.edu>
2023-04-10 22:49:33 -0400

    updated faq page

--- js/faq.js
+++ js/faq.js
@@ -1,31 +1,48 @@
-$(function() {
-
- // Search field filter
-  $("#faq-search").on("keyup", function() {
+$(function () {
+  // Search field filter
+  $("#faq-search").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#faqs .card").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    $("#faqs .card").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
 
-// Search reset button
-$("#search-reset").on("click", function(){
-   // clear the search input
-   $("#faq-search").val("");
-   // Show all the cards
-   $(".card").fadeIn().removeClass("d-none");
-}); 
+  // Search reset button
+  $("#search-reset").on("click", function () {
+    // clear the search input
+    $("#faq-search").val("");
+    // Show all the cards
+    $(".card").fadeIn().removeClass("d-none");
+  });
 
-    // plus and x-mark toggler on opening/closing the accordions
-$(".btn-link").on("click", function() {
-      if ($(this).prev().find("p i").hasClass("fa-plus") && $(this).prev().find("i").hasClass("fa-rotate-45")) {
-        $(this).prev().find("p i").removeClass("fa-rotate-45");
-      } else {
-        $(this).prev().find("p i").addClass("fa-rotate-45");
-      }
+  // plus and x-mark toggler on opening/closing the accordions
+  $(".btn-link").on("click", function () {
+    // console.log($(this).parent().parent().find("p").children("i").classList());
 
-    });
+    if ($(this).parent().parent().find("svg").hasClass("fa-plus") ) {
+      $(this).parent().parent().find("svg").removeClass("fa-plus");
+      $(this).parent().parent().find("svg").addClass("fa-xmark");
+    } else {
+      $(this).parent().parent().find("svg").removeClass("fa-xmark");
+      $(this).parent().parent().find("svg").addClass("fa-plus");
+    }
+  });
 
+  // open all accordion panels for possible printing or close -----------
+  $("#faq-show-all").on("click", function () {
+    console.log("clicked");
 
+    if ($("#faq-show-all").text() === "Open All") {
+      // Change the button text
+      $("#faq-show-all").html("<i class='fa-solid fa-xmark mr-2'></i>Close All</i>");
+      // show all accordions
+      $("#wrapper .collapse").collapse("show");
+    } else {
+      // Change the button text
+      $("#faq-show-all").html("<i class='fa-solid fa-plus mr-2'></i>Open All</i>");
+      // hide all accordions
+      $("#wrapper .collapse").collapse("hide");
+    }
+  });
 });
 // document.ready
